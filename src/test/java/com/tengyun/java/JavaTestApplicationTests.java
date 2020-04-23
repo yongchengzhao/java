56c51b48423ed3abdc50fa7ae62045cc4963aebf
yongchengzhao <yongchengzhao@126.com>
2020-04-23 15:11:54 +0800

    - 加入 Spring Initializr 的内容。

---
+++ src/test/java/com/tengyun/java/JavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tengyun.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
