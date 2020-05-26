c997e53c759b02a2006be88667a346dd613d71e5
yongchengzhao <yongchengzhao@126.com>
2020-05-26 11:48:09 +0800

    日志模板打印器。

---
+++ src/main/java/com/tengyun/script/LogTemplatePrinter.java
@@ -0,0 +1,50 @@
+package com.tengyun.script;
+
+import javax.validation.constraints.Null;
+
+/**
+ * @author Adam
+ */
+public class LogTemplatePrinter {
+
+    public int year = 2020;
+
+    public int weekStart = 1;
+
+    public int weekEnd = 52;
+
+    public String taskPre = "- [ ] ";
+
+    public LogTemplatePrinter(){}
+
+    public void printWeekInfo(int week){
+        System.out.println("### " + year + "年第" + week + "周\n\nTasks:\n");
+    }
+
+    public void printLogTemplate(){
+        for (int i = weekStart; i < weekEnd + 1; i++){
+            printWeekInfo(i);
+            printWalkInfo(i);
+        }
+    }
+
+    public void printWalkInfo(int week){
+        int amount = 0;
+        if (week <= 22){
+        }
+        else if (week <= 36){
+            int perWeek = (12 * 36) / (36 - 22);
+            System.out.println(taskPre + (week - 22) * perWeek + "km.\n");
+        }
+        else if (week <= 52){
+            System.out.println(taskPre + week * 12 + "km.\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("----------main----------");
+        LogTemplatePrinter ltp = new LogTemplatePrinter();
+        ltp.weekStart = 22;
+        ltp.printLogTemplate();
+    }
+}
