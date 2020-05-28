0164d5160f3e6bf13ea877ae7e03e4a533f791ed
yongchengzhao <yongchengzhao@126.com>
2020-05-28 11:22:35 +0800

    测试 toString() 方法。

---
+++ src/main/java/com/tengyun/tostring/TestToString.java
@@ -0,0 +1,35 @@
+package com.tengyun.tostring;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @author Adam
+ */
+public class TestToString {
+    public static void main(String[] args) {
+        Person p1 = new Person("Adam", 24);
+
+        System.out.println(JSON.toJSONString(p1));
+        System.out.println(p1);
+    }
+}
+
+class Person {
+    public String name;
+    public int age;
+
+    public Person(){}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person["
+                + "name=" + this.name
+                + ", age=" + this.age
+                + "]";
+    }
+}
