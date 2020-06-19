34fecd7b411f7fdee606fb6a78aef449f4241d03
yongchengzhao <yongchengzhao@126.com>
2020-06-19 11:03:26 +0800

    练习字符数据类型。

---
+++ src/main/java/com/tengyun/crazyit/chapter03datatype/CharacterTest.java
@@ -0,0 +1,15 @@
+package com.tengyun.crazyit.chapter03datatype;
+
+/**
+ * @author Adam
+ */
+public class CharacterTest {
+    public static void main(String[] args) {
+        int2Char();
+    }
+
+    public static void int2Char() {
+        System.out.println((char)65);
+        System.out.println((char)97);
+    }
+}
