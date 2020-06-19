aa29b6edbb374d23b26cc2b5850cdffe6ff6e41e
yongchengzhao <yongchengzhao@126.com>
2020-06-19 11:40:45 +0800

    进制转换。

--- src/main/java/com/tengyun/crazyit/chapter03datatype/IntegerTest.java
+++ src/main/java/com/tengyun/crazyit/chapter03datatype/IntegerTest.java
@@ -4,4 +4,21 @@ package com.tengyun.crazyit.chapter03datatype;
  * @author Adam
  */
 public class IntegerTest {
+    public static void main(String[] args) {
+        trans2Binary(16);
+        trans2Octal(16);
+        trans2Hexadecimal(16);
+    }
+
+    public static void trans2Binary(int i) {
+        System.out.println(Integer.toBinaryString(i));
+    }
+
+    public static void trans2Octal(int i) {
+        System.out.println(Integer.toOctalString(i));
+    }
+
+    public static void trans2Hexadecimal(int i) {
+        System.out.println(Integer.toHexString(i));
+    }
 }
