26bfd5bfc25128c37cf46071cba8dd40ae2ee5d7
yongchengzhao <yongchengzhao@126.com>
2020-06-19 11:22:48 +0800

    练习字符数据类型。

--- src/main/java/com/tengyun/crazyit/chapter03datatype/CharacterTest.java
+++ src/main/java/com/tengyun/crazyit/chapter03datatype/CharacterTest.java
@@ -5,11 +5,29 @@ package com.tengyun.crazyit.chapter03datatype;
  */
 public class CharacterTest {
     public static void main(String[] args) {
-        int2Char();
+        myName();
     }
 
     public static void int2Char() {
         System.out.println((char)65);
         System.out.println((char)97);
+    }
+
+    public static void myName() {
+
+        System.out.println((int)'赵');
+        System.out.println(0x8d75);
+
+        System.out.println((int)'成');
+        System.out.println(0x6210);
+
+        System.out.println((int)'永');
+        System.out.println(0x6c38);
+
+        System.out.println((int)'腾');
+        System.out.println(0x817e);
+
+        System.out.println((int)'云');
+        System.out.println(0x4e91);
     }
 }
