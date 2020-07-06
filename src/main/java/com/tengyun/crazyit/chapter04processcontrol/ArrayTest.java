34b6d021f1cc9dcc0f81c5d3cb84407b9bb1c18e
yongchengzhao <yongchengzhao@126.com>
2020-07-06 16:31:44 +0800

    遍历数组。

--- src/main/java/com/tengyun/crazyit/chapter04processcontrol/ArrayTest.java
+++ src/main/java/com/tengyun/crazyit/chapter04processcontrol/ArrayTest.java
@@ -5,13 +5,20 @@ package com.tengyun.crazyit.chapter04processcontrol;
  */
 public class ArrayTest {
     public static void main(String[] args) {
+        initTwoDimensionArray();
+    }
+
+    /**
+     * 遍历数组。
+     */
+    private static void traversalArray() {
         int[][] intInt = new int[4][2];
         System.out.println(intInt.length);
         System.out.println(intInt[0].length);
 
-        for (int i = 0; i < intInt.length; i++) {
-            for (int j = 0; j < intInt[i].length; j++) {
-                System.out.print(intInt[i][j] + ", ");
+        for (int[] intArray : intInt) {
+            for (int anInt : intArray) {
+                System.out.print(anInt + ", ");
             }
             System.out.println();
         }
