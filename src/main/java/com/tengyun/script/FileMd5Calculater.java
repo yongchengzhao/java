64108015068b6e2b1cc41bb2b686e0ce12437f09
yongchengzhao <yongchengzhao@126.com>
2020-07-29 16:40:44 +0800

    加入注释信息。

--- src/main/java/com/tengyun/script/FileMd5Calculater.java
+++ src/main/java/com/tengyun/script/FileMd5Calculater.java
@@ -1,19 +1,28 @@
 package com.tengyun.script;
 
-
-
-import org.springframework.util.DigestUtils;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Set;
+import java.util.HashSet;
+import org.springframework.util.DigestUtils;
 
 /**
  * @author Adam
  */
 public class FileMd5Calculater {
-    public void getFilePathListByPath(String dir, ArrayList<String> filePathList) {
+
+    /**
+     * 根据传入的文件夹路径使用递归算法获取该路径下所有的文件路径列表。
+     * @param dir 根路径
+     * @param filePathList 保存子文件路径的列表
+     */
+    public void getFilePathListByPath(String dir, List<String> filePathList) {
         File parentDir = new File(dir);
 
         for (File file: Objects.requireNonNull(parentDir.listFiles())) {
@@ -26,7 +35,13 @@ public class FileMd5Calculater {
         }
     }
 
-    public Map<String, String> getFilePathAndMd5MapByFilePathList(ArrayList<String> filePathList) throws IOException {
+    /**
+     * 根据文件路径列表生成路径和 MD5 值键值对，键为路径，值为 MD5 值。
+     * @param filePathList 子文件路径列表
+     * @return 子文件路径和 MD5 值键值对 map
+     * @throws IOException 不处理异常
+     */
+    public Map<String, String> getFilePathAndMd5MapByFilePathList(List<String> filePathList) throws IOException {
         Map<String, String> filePathAndMd5Map = new HashMap<>(16);
         for(String filePath: filePathList){
             String md5Value = getMd5ByFilePath(filePath);
@@ -36,21 +51,31 @@ public class FileMd5Calculater {
         return filePathAndMd5Map;
     }
 
+    /**
+     * 根据文件路径生成并返回该文件的 MD5 值。
+     * @param filePath 文件路径
+     * @return MD5 值
+     * @throws IOException 不处理异常
+     */
     private String getMd5ByFilePath(String filePath) throws IOException {
         String md5Value = DigestUtils.md5DigestAsHex(new FileInputStream(filePath));
         System.out.println("md5Value: " + md5Value);
+
         return md5Value;
     }
 
     public static void main(String[] args) throws IOException {
+        // 准备数据，设置需要遍历的文件夹根路径
         FileMd5Calculater fmc = new FileMd5Calculater();
         String rootPath = "E:\\Adam\\book\\李笑来\\定投改变命运";
-        ArrayList<String> filePathList = new ArrayList<>();
+        List<String> filePathList = new ArrayList<>();
 
+        // 获取子文件路径列表并遍历
         fmc.getFilePathListByPath(rootPath, filePathList);
         filePathList.forEach(System.out::println);
         System.out.println("filePathList.size(): " + filePathList.size());
 
+        // 获取子文件路径和 MD5 值 map 并遍历。将所有 MD5 值存入 set 用于去重，并遍历该 set。
         Map<String, String> filePathAndMd5Map = fmc.getFilePathAndMd5MapByFilePathList(filePathList);
         Set<String> md5Set = new HashSet<>();
         filePathAndMd5Map.forEach((key, value) -> {
